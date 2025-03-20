a2de0462e1f13ceec952352b10942bbecc942edc
enurisamaraweera <enuri.samaraweera@tum.de>
2025-03-20 11:59:20 +0100

    Added Tests

---
+++ src/main/java/main/Bank.java
@@ -0,0 +1,65 @@
+package main;
+
+import java.util.List;
+
+public class Bank {
+    private List<Customer> customers;
+    private List<Account> accounts;
+
+    public Bank(List<Customer> customers, List<Account> accounts) {
+        this.customers = customers;
+        this.accounts = accounts;
+    }
+    public float checkBalance(String accountId) {
+        Account account = accounts.stream().filter(account1 -> account1.getAccountId().equals(accountId)).findFirst().orElse(null);
+        if (account == null) {
+            System.out.println("Invalid AccountID");
+            return Float.parseFloat(null);
+        }
+        else return account.getBalance();
+
+    }
+
+    public void depositMoney(String accountId, float amount) {
+        Account account = accounts.stream().filter(account1 -> account1.getAccountId().equals(accountId)).findFirst().orElse(null);
+        if (account == null) {
+            System.out.println("Invalid AccountID");
+        }
+        else if (amount > 100000F){
+            System.out.println("Amount is above maximum limit. Please contact bank manager");
+        }
+        else {
+            account.setBalance(account.getBalance() + amount);
+            System.out.println("Transaction successful!");
+        }
+
+    }
+
+    public void withdrawMoney (String accountId, float amount) {
+        Account account = accounts.stream().filter(account1 -> account1.getAccountId().equals(accountId)).findFirst().orElse(null);
+        if (account == null) {
+            System.out.println("Invalid AccountID");
+        } else if (account.getBalance() < amount) {
+            System.out.println("Insufficient Balance");
+
+        } else if (account.isFixedDeposit()) {
+            System.out.println("Cannot withdraw money from a Fixed Deposit Account");
+
+        } else {
+            account.setBalance(account.getBalance() - amount);
+            System.out.println("Transaction successful!");
+        }
+
+
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Bank!");
+    }
+}
